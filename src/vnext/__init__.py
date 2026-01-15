da63b06c6f954de1634b534e4c723af7cb4336c3
Pete Peterson <petersonpf@ornl.gov>
2026-01-15 17:40:31 -0500

    Replace versioningit with static versions

--- src/vnext/__init__.py
+++ src/vnext/__init__.py
@@ -1,9 +1,9 @@
 """Contains the entry point for the application"""
 
-try:
-    from ._version import __version__  # noqa: F401
-except ImportError:
-    __version__ = "unknown"
+from importlib import metadata
+
+__version__ = metadata.version("postprocessing")
+del metadata
 
 
 def VNext():  # noqa N802
