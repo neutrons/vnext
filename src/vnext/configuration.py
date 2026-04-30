1a15f4b59fd39da93187acfd2c6c8197bd088359
Pete Peterson <petersonpf@ornl.gov>
2026-04-30 12:41:56 -0400

    Rewrite the configuration object so tests can override path to calibration files (#32)
    This is the next step in getting #30 ready to use. `vnext.Configuration`
    is modified to have a single value accessible and adds the capability to
    override that value on construction to allow for writing tests to run on
    places with a different location for the calibration files.
    
    `vnext.Configuration` now inherits from python's
    [`configparser.ConfigParser`](https://docs.python.org/3/library/configparser.html).
    
    ## Manual test for the reviewer
    After seeing that the tests pass, one can try using the class directly.
    
    ## References
    
    
    [EWM16107](https://ornlrse.clm.ibmcloud.com/ccm/web/projects/Neutron%20Data%20Project%20(Change%20Management)#action=com.ibm.team.workitem.viewWorkItem&id=16107)

--- src/vnext/configuration.py
+++ src/vnext/configuration.py
@@ -3,103 +3,42 @@
 Will fall back to a default
 """
 
-import os
-import shutil
 from configparser import ConfigParser
 from pathlib import Path
 
 from mantid.kernel import Logger
 
-logger = Logger("PACKAGENAME")
-
-# configuration settings file path
-CONFIG_PATH_FILE = os.path.join(Path.home(), ".vnext", "configuration.ini")
+# configuration settings file path - this is where linux puts things by default
+CONFIG_PATH_USER: Path = Path.home() / ".config" / "vnext" / "configuration.ini"
 
 
-class Configuration:
+class Configuration(ConfigParser):
     """Load and validate Configuration Data"""
 
-    def __init__(self):
-        """Initialization of configuration mechanism"""
-        # capture the current state
-        self.valid = False
+    _log = Logger("vnext.Configuration")
 
-        # locate the template configuration file
-        project_directory = Path(__file__).resolve().parent
-        self.template_file_path = os.path.join(project_directory, "configuration_template.ini")
-
-        # retrieve the file path of the file
-        self.config_file_path = CONFIG_PATH_FILE
-        logger.information(f"{self.config_file_path} will be used")
-
-        # if template conf file path exists
-        if os.path.exists(self.template_file_path):
-            # file does not exist create it from template
-            if not os.path.exists(self.config_file_path):
-                # if directory structure does not exist create it
-                if not os.path.exists(os.path.dirname(self.config_file_path)):
-                    os.makedirs(os.path.dirname(self.config_file_path))
-                shutil.copy2(self.template_file_path, self.config_file_path)
-
-            self.config = ConfigParser(allow_no_value=True, comment_prefixes="/")
-            # parse the file
-            try:
-                self.config.read(self.config_file_path)
-                # validate the file has the all the latest variables
-                self.validate()
-            except ValueError as err:
-                logger.error(str(err))
-                logger.error(f"Problem with the file: {self.config_file_path}")
+    def __init__(self, filename: Path = CONFIG_PATH_USER, **kwargs):
+        """Initialization of configuration mechanism
+        :param filename: path to the configuration file, defaults to CONFIG_PATH_USER
+        :param kwargs: optional overrides for configuration values, in the form of section.key=value"""
+        ConfigParser.__init__(self)
+        if filename.exists():
+            self._log.debug(f"Loading configuration from {filename}")
+            self.read(filename)
         else:
-            logger.error(f"Template configuration file: {self.template_file_path} is missing!")
+            self._log.debug(f"Configuration {filename} does not exist, loading defaults")
 
-    def validate(self):
-        """Validates that the fields exist at the config_file_path and writes any missing fields/data
-        using the template configuration file: configuration_template.ini as a guide
-        """
-        template_config = ConfigParser(allow_no_value=True, comment_prefixes="/")
-        template_config.read(self.template_file_path)
-        for section in template_config.sections():
-            # if section is missing
-            if section not in self.config.sections():
-                # copy the whole section
-                self.config.add_section(section)
+        # override with provided kwargs in the form of section.key=value
+        for key, value in kwargs.items():
+            section, key = key.split(".", 1)
+            self._log.debug(f"Overriding configuration: {section} {key}={value}")
+            if not self.has_section(section):
+                self.add_section(section)
+            self.set(section, key, value)
 
-            for item in template_config.items(section):
-                field, _ = item
-                if field not in self.config[section]:
-                    # copy the field
-                    self.config[section][field] = template_config[section][field]
-        with open(self.config_file_path, "w", encoding="utf8") as config_file:
-            self.config.write(config_file)
-        self.valid = True
-
-    def is_valid(self):
-        """Returns the configuration state"""
-        return self.valid
-
-
-def get_data(section, name=None):
-    """Retrieves the configuration data for a variable with name"""
-    # default file path location
-    config_file_path = CONFIG_PATH_FILE
-    if os.path.exists(config_file_path):
-        config = ConfigParser()
-        # parse the file
-        config.read(config_file_path)
-        try:
-            if name:
-                value = config[section][name]
-                # in case of boolean string value cast it to bool
-                if value in ("True", "False"):
-                    return value == "True"
-                # in case of None
-                if value == "None":
-                    return None
-                return value
-            return config[section]
-        except KeyError as err:
-            # requested section/field do not exist
-            logger.error(str(err))
-            return None
-    return None
+    def get_calibration_path(self) -> Path:
+        """Get the path to the calibration files"""
+        path = Path(self.get("Paths", "calibration", fallback="/SNS/VULCAN/shared/CALIBRATION/"))
+        # user path might be in the path, so expand it
+        path = path.expanduser()
+        return path
---
+++ tests/test_configuration.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from vnext import Configuration
+
+
+def test_config():
+    config = Configuration()
+    assert config
+    assert config.get_calibration_path()
+
+
+def test_config_override():
+    config = Configuration(**{"Paths.calibration": "/tmp/calibration"})
+
+    assert config.get_calibration_path() == Path("/tmp/calibration")
