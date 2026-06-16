b3190e854a5d590dc6f9b892cfeb573a0af1eeef
rboston628 <bostonsr@ornl.gov>
2026-06-16 11:10:02 -0400

    doc for DAO

--- src/vnext/dao/__init__.py
+++ src/vnext/dao/__init__.py
@@ -1,3 +1,7 @@
+# DAO (Data Access Object) classes for vNext.  These are simple dataclasses that
+# represent the data structures needed to perform reduction, and functions to
+# load them from the bundled YAML files.
+
 from .calibration_files import CalibrationFiles
 from .focus_positions import FocusPositions
 from .tof_bins import TofBins
