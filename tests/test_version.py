83adb29e40bd92d49e5cbf0d90b1982ef2c3ed95
rboston628 <bostonsr@ornl.gov>
2026-09-03 17:01:05 -0400

    patch

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,8 +2,4 @@ from vnext import __version__
 
 
 def test_version():
-    assert __version__ == "unknown" or "dev" in __version__ or "0.1.0" in __version__
-    #      ^ conda env will default to "unknown" if not set
-    #                                  ^ pixi will default to the default tag in pryproject.toml
-    #                                    if not set, it will default to "0.0.1" + "devxxx" on local dev builds
-    #                                                          ^ this is the default version on runner
+    assert "rc" in __version__ or "dev" in __version__ or "0.1.0" in __version__
